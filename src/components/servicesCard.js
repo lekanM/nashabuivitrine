c05244b4214dc3d036dd85fb1ee19a14b0cc7689
lekanM <micklekan@gmail.com>
2022-11-24 00:14:23 +0100

    fixed

--- src/components/servicesCard.js
+++ src/components/servicesCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const servicesCard = ({path,title,serviceDetail}) => {
   return (
     <div className='card'>
-      <img src={path} />
+      <img src={path} alt='hi'/>
       <section>
         <h3>{title}</h3>
         <p>{serviceDetail}</p>
